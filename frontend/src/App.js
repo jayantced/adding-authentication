794f47c0a4af93b62c498c362da8d9d435052604
jayantced739 <109576199+jayantced@users.noreply.github.com>
2023-12-27 00:00:10 +0530

    complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,10 +13,15 @@ import NewEventPage from './pages/NewEvent';
 import RootLayout from './pages/Root';
 import { action as manipulateEventAction } from './components/EventForm';
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
+import AuthenticationPage, { action as authAction } from './pages/Authentication';
+import { action as logoutAction } from './pages/Logout';
+import { checkAuthLoader, tokenLoader } from './util/auth';
 
 const router = createBrowserRouter([
   {
     path: '/',
+    id: 'root',
+    loader: tokenLoader,
     element: <RootLayout />,
     errorElement: <ErrorPage />,
     children: [
@@ -44,6 +49,7 @@ const router = createBrowserRouter([
                 path: 'edit',
                 element: <EditEventPage />,
                 action: manipulateEventAction,
+                loader: checkAuthLoader
               },
             ],
           },
@@ -51,13 +57,23 @@ const router = createBrowserRouter([
             path: 'new',
             element: <NewEventPage />,
             action: manipulateEventAction,
+            loader: checkAuthLoader
           },
         ],
+      },
+      {
+        path: 'auth',
+        element: <AuthenticationPage />,
+        action: authAction
       },
       {
         path: 'newsletter',
         element: <NewsletterPage />,
         action: newsletterAction,
+      },
+      {
+        path: 'logout',
+        action: logoutAction,
       },
     ],
   },
